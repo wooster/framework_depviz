1ce00c2bbb3ebcbc213f683905c219cc45dc7d8f
Andrew Wooster <wooster_spam@nextthing.org>
2013-08-12 15:06:13 -0700

    Works now.

--- README.md
+++ README.md
@@ -10,6 +10,19 @@ Example usage:
 
 This will create a file named `dependencies.dot` which can be opened with Graphviz.
 
+You might also do:
+
+`./depviz.py "~/Library/Developer/Xcode/iOS DeviceSupport/5.1.1 (9B206)/Symbols/System/Library/" AirTraffic > airtraffic.dot`
+
+To generate a graph of just the dependencies of `AirTraffic.framework`. This graph might look like:
+
+![AirTraffic Graph](screenshots/airtraffic.png?raw=true)
+
+
+You can append multiple names, eg:
+
+`./depviz.py "~/Library/Developer/Xcode/iOS DeviceSupport/5.1.1 (9B206)/Symbols/System/Library/" AirTraffic CoreVideo`
+
 ## Installing Graphviz on OS X
 
 The most reliable method for installing Graphviz on OS X I've found is with Homebrew:
@@ -32,6 +45,6 @@ You may use the application, although it will hang for a long time generating gr
 
 I usually find it nicer to use the command line, eg:
 
-`dot -v -Tpdf -o deps.pdf deps.dot`
+`dot -v -Gnslimit=20 -Gmaxiter=10 -Gmaxiter1=10 -Tpdf -o deps.pdf deps.dot`
 
 Which will generate a PDF of the output called `deps.pdf`.
--- depviz.py
+++ depviz.py
@@ -25,13 +25,31 @@ DEPENDENCY_PRIVATE = 'Private'
 DEPENDENCY_PUBLIC = 'Public'
 DEPENDENCY_UNKNOWN = 'Unknown'
 
-def dependencies_for_framework_path(framework_path, exclude_dylibs=True):
-    (path, filename) = os.path.split(framework_path)
+def dependencies_for_resolved_framework_path(lib_base, framework_path, dependencies, dep_to_visibility, exclude_dylibs=True):
+    def visibility_from_path(path):
+        visibility = DEPENDENCY_UNKNOWN
+        if '/PrivateFrameworks/' in path:
+            visibility = DEPENDENCY_PRIVATE
+        elif '/Frameworks/' in path:
+            visibility = DEPENDENCY_PUBLIC
+        return visibility
+    
+    real_framework_path = framework_path
+    if not framework_path.startswith(lib_base):
+        real_framework_path = lib_base + framework_path
+        if not os.path.exists(real_framework_path):
+            real_framework_path = framework_path
+    if not os.path.exists(real_framework_path):
+        print >> sys.stderr, "Unable to find framework:", real_framework_path
+        return
+    
+    (path, filename) = os.path.split(real_framework_path)
     (base, ext) = os.path.splitext(filename)
-    lib_path = os.path.join(framework_path, base)
-    (status, output) = run_command("otool -L %s" % escape_arg(lib_path))
+    (status, output) = run_command("otool -L %s" % escape_arg(real_framework_path))
     lines = output.splitlines()
-    dependencies = []
+    
+    dep_to_visibility[base] = visibility_from_path(real_framework_path)
+    
     for line in lines:
         if not line.startswith("\t"):
             continue
@@ -45,15 +63,30 @@ def dependencies_for_framework_path(framework_path, exclude_dylibs=True):
         if depname == base:
             # Exclude self-dependency.
             continue
-        visibility = DEPENDENCY_UNKNOWN
+        visibility = visibility_from_path(f_path)
         if exclude_dylibs and f_path.endswith("dylib"):
             continue
-        if '/PrivateFrameworks/' in f_path:
-            visibility = DEPENDENCY_PRIVATE
-        elif '/Frameworks/' in f_path:
-            visibility = DEPENDENCY_PUBLIC
-        dependencies.append((depname, visibility))
-    return (base, dependencies)
+        
+        should_recurse = (dep_to_visibility.get(depname) is None)
+        dep_to_visibility[depname] = visibility
+        dependencies.setdefault(base, [])
+        if not depname in dependencies[base]:
+            dependencies[base].append(depname)
+        if should_recurse:
+            dependencies_for_resolved_framework_path(lib_base, f_path, dependencies, dep_to_visibility, exclude_dylibs=exclude_dylibs)
+
+def dependencies_for_framework_path(framework_path, dependencies, dep_to_visibility, exclude_dylibs=True):
+    (path, filename) = os.path.split(framework_path)
+    (base, ext) = os.path.splitext(filename)
+    lib_path = os.path.join(framework_path, base)
+    lib_parts = lib_path.split(os.sep)
+    lib_base_parts = []
+    for part in lib_parts:
+        if part == "System":
+            break
+        lib_base_parts.append(part)
+    lib_base = os.sep.join(lib_base_parts)
+    return dependencies_for_resolved_framework_path(lib_base, lib_path, dependencies, dep_to_visibility, exclude_dylibs=exclude_dylibs)
 
 def dependencies_for_system_library_path(library_path):
     entries = os.listdir(library_path)
@@ -68,21 +101,12 @@ def dependencies_for_system_library_path(library_path):
             if not framework_path.endswith(".framework"):
                 continue
             full_path = os.path.join(library_path, f_path, framework_path)
-            (framework_name, framework_dependencies) = dependencies_for_framework_path(full_path)
-            if "/PrivateFrameworks/" in framework_path:
-                dep_to_visibility[framework_name] = DEPENDENCY_PRIVATE
-            elif "/Frameworks/" in framework_path:
-                dep_to_visibility[framework_name] = DEPENDENCY_PUBLIC
-            dependencies.setdefault(framework_name, [])
-            for dep in framework_dependencies:
-                dependencies[framework_name].append(dep)
-                (depname, visibility) = dep
-                dep_to_visibility[depname] = visibility
+            dependencies_for_framework_path(full_path, dependencies, dep_to_visibility)
     update_dependencies(dependencies, dep_to_visibility, library_path, "Frameworks")
     update_dependencies(dependencies, dep_to_visibility, library_path, "PrivateFrameworks")
     return (dependencies, dep_to_visibility)
 
-def dot_for_dependencies(dependencies, dep_to_visibility):
+def dot_for_dependencies(dependencies, dep_to_visibility, framework_depnames=None):
     l = []
     l.append("digraph G {")
     l.append("\tnode [shape=box];")
@@ -95,48 +119,94 @@ def dot_for_dependencies(dependencies, dep_to_visibility):
         else:
             return "#FFFFFF"
     
-    nodes = {}
-    seen_deps = []
-    i = 0
-    for framework_name, visibility in dep_to_visibility.iteritems():
-        if framework_name in seen_deps:
-            continue
-        nodename = "Node%d" % i
-        i += 1
-        nodes[framework_name] = nodename
-        seen_deps.append(framework_name)
-        color = color_for_visibility(dep_to_visibility[framework_name])
-        l.append("%s [label=\"%s\", fillcolor=\"%s\"];" % (nodename, framework_name, color))
-    print nodes
-    #for framework_name, _ in dependencies.iteritems():
-    #    if nodes.get(framework_name) is None:
-    #        nodename = "Node%d" % i
-    #        i += 1
-    #        nodes[framework_name] = nodename
-    #        seen_deps.append(framework_name)
-    #        color = color_for_visibility(dep_to_visibility[framework_name])
-    #        l.append("%s [label=\"%s\", fillcolor=\"%s\"];" % (nodename, framework_name, color))
     
-    for framework_name, deps in dependencies.iteritems():
-        if nodes.get(framework_name) is None:
-            print >> sys.stderr, "Unknown node", framework_name
-            continue
-        from_nodename = nodes[framework_name]
-        if len(deps) == 0:
-            l.append("\t\"%s\" -> {};" % framework_name)
-        for (lib_name, visibility) in deps:
-            to_nodename = nodes[lib_name]
-            l.append("\t%s -> %s;" % (from_nodename, to_nodename))
+    if framework_depnames is None:
+        nodes = {}
+        seen_deps = []
+        i = 0
+        for framework_name, visibility in dep_to_visibility.iteritems():
+            if framework_name in seen_deps:
+                continue
+            nodename = "Node%d" % i
+            i += 1
+            nodes[framework_name] = nodename
+            seen_deps.append(framework_name)
+            color = color_for_visibility(dep_to_visibility[framework_name])
+            l.append("\t%s [label=\"%s\", fillcolor=\"%s\"];" % (nodename, framework_name, color))
+        for framework_name, deps in dependencies.iteritems():
+            if nodes.get(framework_name) is None:
+                print >> sys.stderr, "Unknown node", framework_name
+                continue
+            from_nodename = nodes[framework_name]
+            if len(deps) == 0:
+                l.append("\t\"%s\" -> {};" % framework_name)
+            for lib_name in deps:
+                to_nodename = nodes[lib_name]
+                l.append("\t%s -> %s; // %s -> %s" % (from_nodename, to_nodename, framework_name, lib_name))
+    else:
+        def gather_dependents(dependencies, framework_name, seen=None):
+            """Get a list of all the frameworks wich depend on framework_name, recursively."""
+            results = []
+            if seen is None:
+                seen = []
+            for framework, deps in dependencies.iteritems():
+                if framework_name in deps:
+                    if framework in seen:
+                        continue
+                    seen.append(framework)
+                    # framework depends on framework_name
+                    results.append(framework_name)
+                    for result in gather_dependents(dependencies, framework, seen=seen):
+                        results.append(result)
+            return list(set(results))
+        frameworks_to_print = []
+        for framework_depname in framework_depnames:
+            for f in gather_dependents(dependencies, framework_depname):
+                frameworks_to_print.append(f)
+        frameworks_to_print = list(set(frameworks_to_print))
+        nodes = {}
+        seen_deps = []
+        i = 0
+        for framework_name, visibility in dep_to_visibility.iteritems():
+            if framework_name in seen_deps:
+                continue
+            if framework_name not in frameworks_to_print:
+                continue
+            nodename = "Node%d" % i
+            i += 1
+            nodes[framework_name] = nodename
+            seen_deps.append(framework_name)
+            color = color_for_visibility(dep_to_visibility[framework_name])
+            l.append("\t%s [label=\"%s\", style=filled, fillcolor=\"%s\"];" % (nodename, framework_name, color))
+        for framework_name, deps in dependencies.iteritems():
+            if framework_name in frameworks_to_print:
+                if nodes.get(framework_name) is None:
+                    print >> sys.stderr, "Unknown node", framework_name
+                    continue
+                from_nodename = nodes[framework_name]
+                if len(deps) == 0:
+                    l.append("\t\"%s\" -> {};" % framework_name)
+                for lib_name in deps:
+                    if lib_name in frameworks_to_print:
+                        to_nodename = nodes[lib_name]
+                        l.append("\t%s -> %s; // %s -> %s" % (from_nodename, to_nodename, framework_name, lib_name))
+                
     l.append("}\n")
     return "\n".join(l)
 
-def main(library_path):
+def main(library_path, framework_depnames=None):
+    library_path = os.path.expanduser(library_path)
     (dependencies, dep_to_visibility) = dependencies_for_system_library_path(library_path)
-    dot_output = dot_for_dependencies(dependencies, dep_to_visibility)
+    dot_output = dot_for_dependencies(dependencies, dep_to_visibility, framework_depnames=framework_depnames)
     print >> sys.stdout, dot_output
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print >> sys.sterr, "Usage: %s [SDK System Library Path]"
+        print >> sys.stderr, "Usage: %s [SDK System Library Path] [framework name ...]"
+        print >> sys.stderr, "  Where the library path is like /System/Library"
+        print >> sys.stderr, "  Where the framework name (optional) is one to determine what depends on it"
         sys.exit(1)
-    main(sys.argv[1])
+    framework_depnames = None
+    if len(sys.argv) > 2:
+        framework_depnames = sys.argv[2:]
+    main(sys.argv[1], framework_depnames=framework_depnames)
